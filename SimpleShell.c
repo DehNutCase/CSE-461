0e395cde7381689c4f0db55f997c4b5c0b267146
006198682 <006198682@coyote.csusb.edu>
2020-04-09 13:52:25 -0700

    minor edits

--- SimpleShell.c
+++ SimpleShell.c
@@ -33,7 +33,7 @@ void read_command (char cmd[], char *par[])
 void type_prompt()
 {
     static int first_time = 1;
-    if (frst_time){
+    if (first_time){
         const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
         write(STDOUT_FILENO,CLEAR_SCREEN_ANSI, 12);
         first_time = 0;
